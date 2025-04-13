dda0ac6df1663375b988987f04392209a80cd60f
Max <maksymkosfe.developer@gmail.com>
2025-04-13 14:37:49 +0300

    commit

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,89 +1,99 @@
+'use client'
 import Image from "next/image";
 import Link from 'next/link';
 
+import { useState } from 'react'
+import {
+    Dialog,
+    DialogPanel,
+    PopoverGroup,
+} from '@headlessui/react'
+import {
+    Bars3Icon,
+    XMarkIcon,
+} from '@heroicons/react/24/outline'
+
 export default function Header() {
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
     return (
-        <header className="bg-white fixed top-0 left-0 w-full border-b border-gray-200 z-50">
-            <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-                <div className="flex h-24 items-center justify-between">
-                    <div className="flex-1 md:flex md:items-center md:gap-12"> 
-                        <a className="block text-teal-600" href="#">
-                            <span className="sr-only">Home</span>
-                            <Image src="/images/logo.png" width={120} height={120} alt='logo' />
-                        </a>
-                    </div>
-
-                    <div className="md:flex md:items-center md:gap-12">
-                        <nav aria-label="Global" className="hidden md:block">
-                            <ul className="flex items-center gap-6 text-lg">
-                                <li>
-                                    <Link href="/" className="text-gray-500 transition hover:text-gray-500/75">
-                                        Головна
-                                    </Link>
-                                </li>
-
-                                <li>
-                                    <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Каталог </a>
-                                </li>
-
-                                <li>
-                                    <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Відгуки </a>
-                                </li>
-
-                                <li>
-                                    <a className="text-gray-500 transition hover:text-gray-500/75" href="/portfolio/"> Портфоліо </a>
-                                </li>
-
-                                <li>
-                                    <a className="text-gray-500 transition hover:text-gray-500/75" href="/contact/"> Контакти </a>
-                                </li>
-
-                                <li>
-                                    <a className="text-gray-500 transition hover:text-gray-500/75" href="#"> Про мене </a>
-                                </li>
-                            </ul>
-                        </nav>
-
-                        {/* <div className="flex items-center gap-4">
-                            <div className="sm:flex sm:gap-4">
-                                <a
-                                    className="rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow-sm"
-                                    href="#"
-                                >
-                                    Login
-                                </a>
-
-                                <div className="hidden sm:flex">
-                                    <a
-                                        className="rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-teal-600"
-                                        href="#"
-                                    >
-                                        Register
-                                    </a>
-                                </div>
-                            </div>
-
-                            <div className="block md:hidden">
-                                <button
-                                    className="rounded-sm bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
-                                >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="size-5"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        stroke="currentColor"
-                                        stroke-width="2"
-                                    >
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-                                    </svg>
-                                </button>
-                            </div>
-                        </div> */}
-                    </div>
+        <header className="bg-white border-b border-gray-300 fixed top-0 left-0 z-10 w-full">
+            <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between px-4 py-2 lg:px-8">
+                <div className="flex lg:flex-1">
+                    <a href="#" className="-m-1.5 p-1.5">
+                        <Image
+                            src="/images/logo.png"
+                            width={120}
+                            height={120}
+                            alt='logo'
+                            quality={100}
+                        />
+                    </a>
                 </div>
-            </div>
+                <div className="flex lg:hidden">
+                    <button
+                        type="button"
+                        onClick={() => setMobileMenuOpen(true)}
+                        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <Bars3Icon aria-hidden="true" className="size-9 cursor-pointer" />
+                    </button>
+                </div>
+                <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+                    <Link href="/" className="text-gray-700 transition font-semibold hover:text-gray-500/75">Головна</Link>
+                    <a className="text-gray-700 transition font-semibold hover:text-gray-500/75" href="#"> Каталог </a>
+                    <a className="text-gray-700 transition font-semibold hover:text-gray-500/75" href="#"> Відгуки </a>
+                    <Link className="text-gray-700 transition font-semibold hover:text-gray-500/75" href="/portfolio/"> Портфоліо </Link>
+                    <Link className="text-gray-700 transition font-semibold hover:text-gray-500/75" href="/contact/"> Контакти </Link>
+                    <a className="text-gray-700 transition font-semibold hover:text-gray-500/75" href="#"> Про мене </a>
+                </PopoverGroup>
+                <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+                    <a href="#" className="text-sm/6 font-semibold text-gray-900">
+                        Log in <span aria-hidden="true">&rarr;</span>
+                    </a>
+                </div>
+            </nav>
+            <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+                <div className="fixed inset-0 z-10" />
+                <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="flex items-center justify-between">
+                        <a href="#" className="-m-1.5 p-1.5">
+                            <Image
+                                src="/images/logo.png"
+                                width={100}
+                                height={100}
+                                alt='logo'
+                                quality={100}
+                            />
+                        </a>
+                        <button
+                            type="button"
+                            onClick={() => setMobileMenuOpen(false)}
+                            className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                        >
+                            <span className="sr-only">Close menu</span>
+                            <XMarkIcon aria-hidden="true" className="size-9 cursor-pointer" />
+                        </button>
+                    </div>
+                    <div className="mt-6 flow-root">
+                        <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="space-y-2 py-6">
+                                <Link href="/" className="-mx-3 block rounded-lg px-3 py-2 text-base/7 text-gray-900 hover:bg-gray-50">Головна</Link>
+                                <a className="-mx-3 block rounded-lg px-3 py-2 text-base/7 text-gray-900 hover:bg-gray-50" href="#"> Каталог </a>
+                                <a className="-mx-3 block rounded-lg px-3 py-2 text-base/7 text-gray-900 hover:bg-gray-50" href="#"> Відгуки </a>
+                                <Link className="-mx-3 block rounded-lg px-3 py-2 text-base/7 text-gray-900 hover:bg-gray-50" href="/portfolio/"> Портфоліо </Link>
+                                <Link className="-mx-3 block rounded-lg px-3 py-2 text-base/7 text-gray-900 hover:bg-gray-50" href="/contact/"> Контакти </Link>
+                                <a className="-mx-3 block rounded-lg px-3 py-2 text-base/7 text-gray-900 hover:bg-gray-50" href="#"> Про мене </a>
+                            </div>
+                            <div className="py-6">
+                                <a href="#" className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold text-gray-900 hover:bg-gray-50">
+                                    Log in
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </DialogPanel>
+            </Dialog>
         </header>
-
     )
 }
--- src/components/PageBanner/pagebannner.tsx
+++ src/components/PageBanner/pagebannner.tsx
@@ -1,15 +1,26 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function PageBanner() {
   return (
     <section className="h-[90vh] relative flex items-center justify-center">
-      <Image
+      {/* <Image
         className="h-full w-full object-cover absolute top-0 left-0"
-        src="/homepage-background.jpeg"
-        width={1920}
-        height={1000}
+        src="/images/test1.jpg"
+        fill={true}
         alt="background"
-      />
+        priority={true}
+        quality={100}
+      /> */}
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="h-full w-full object-cover absolute top-0 left-0"
+      >
+        <source src="/videos/large.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <div className="relative text-center">
         <h1 className="text-9xl font-bold text-[#90e0ef]">We bake to make YOU happy</h1>
       </div>
